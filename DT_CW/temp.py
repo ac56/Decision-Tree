b9300ed890747a4472487af2ec2ab8e4cddb8da3
Alexandros Constantinou <52408185+ac56@users.noreply.github.com>
2019-07-22 18:31:47 +0300

    Update temp.py

--- DT_CW/temp.py
+++ DT_CW/temp.py
@@ -4,34 +4,37 @@ import math
 import time
 
 def find_potential_splits(current_df):
+	previous_value = current_df.iloc[0,0]
 	previous_label = current_df.iloc[0,1]
 	potential_splits = []
 	for index, row in current_df.iterrows():
-			if(previous_label != row['label']):
+			if((previous_label != row['label']) and (previous_value != current_df.iloc[index, 0])):
 				previous_label = row['label']
 				potential_splits.append(index)
+			previous_value = current_df.iloc[index, 0]
+			previous_label = row['label']
 	return potential_splits
 
 
 def find_current_best_split(current_df):
 	#global labels_total
 	global label
-	print('Ready to enter find_potential_splits function...\n')
+	#print('Ready to enter find_potential_splits function...\n')
 	potential_splits = find_potential_splits(current_df)
-	print('list of potential splits in current dataframe:')
-	print(potential_splits)
-	print('\n')
+	#print('list of potential splits in current dataframe:')
+	#print(potential_splits)
+	#print('\n')
 	if(not potential_splits):
-		best_split = len(current_df) - 1
-		return best_split, 0
+		return (-1), (-1)
 	else:
 		best_split = potential_splits[0]
+
 	least_curr_entropy = 0
 	first = True
 
 	for current_split in potential_splits:
-		print('split under consideration:')
-		print(current_split)
+		#print('split under consideration:')
+		#print(current_split)
 		curr_split_entropy = 0
 		l_split_entr = 0
 		r_split_entr = 0
@@ -47,26 +50,31 @@ def find_current_best_split(current_df):
 		for current_label in left_label_list:
 			count = temp_left_df['label'].value_counts().loc[current_label]
 			l_split_entr = l_split_entr - (count/left_total_labels)* math.log2((count/left_total_labels))
-			print('There are ' + str(count) + ' ' + str(current_label) + 's in the left temporary dataframe')
-		print('left split entropy is '+str(l_split_entr))
-		print(' (len(temp_left_df)/ len(current_df)) = ' +str( (len(temp_left_df)/ len(current_df))))
+			#print('There are ' + str(count) + ' ' + str(current_label) + 's in the left temporary dataframe')
+		#print('left split entropy is '+str(l_split_entr))
+		#print(' (len(temp_left_df)/ len(current_df)) = ' +str( (len(temp_left_df)/ len(current_df))))
 		curr_split_entropy =  (len(temp_left_df)/ len(current_df)) * l_split_entr
-		print('\n')
+		#print('\n')
 		for current_label in right_label_list:
 			count = temp_right_df['label'].value_counts().loc[current_label]
 			r_split_entr = r_split_entr - (count/right_total_labels)* math.log2((count/right_total_labels))
-			print('There are ' + str(count) + ' ' + str(current_label) + 's in the right temporary dataframe')
-		print('right split entropy is '+str(r_split_entr))
-		print(' (len(temp_right_df)/ len(current_df)) = ' +str( (len(temp_right_df)/ len(current_df))))
+			#print('There are ' + str(count) + ' ' + str(current_label) + 's in the right temporary dataframe')
+		#print('right split entropy is '+str(r_split_entr))
+		#print(' (len(temp_right_df)/ len(current_df)) = ' +str( (len(temp_right_df)/ len(current_df))))
 		curr_split_entropy +=  (len(temp_right_df)/ len(current_df)) * r_split_entr
-		print('*******************The TOTAL entropy of split ' + str(current_split) + ' is \t' + str(curr_split_entropy))
-		if ((curr_split_entropy < least_curr_entropy) and (first == False)):
-			least_curr_entropy = curr_split_entropy
-			best_split = current_split
+		#print('*******************The TOTAL entropy of split ' + str(current_split) + ' is \t' + str(curr_split_entropy))
+		# if ((curr_split_entropy < least_curr_entropy) and (first == False)):
+		# 	least_curr_entropy = curr_split_entropy
+		# 	best_split = current_split
 		if (first == True):
+			best_split = current_split
 			least_curr_entropy = curr_split_entropy
 			first = False
-		print('*********Currently the best split for ' + current_df.columns[0] + ' is at position ' + str(best_split) + ' with an entropy of ' + str(least_curr_entropy) + '\n'	)
+		else:
+			if((curr_split_entropy < least_curr_entropy)):
+				least_curr_entropy = curr_split_entropy
+				best_split = current_split
+		#print('*********Currently the best split for ' + current_df.columns[0] + ' is at position ' + str(best_split) + ' with an entropy of ' + str(least_curr_entropy) + '\n'	)
 	return best_split, least_curr_entropy
 
 
@@ -87,17 +95,17 @@ def find_split(train_df):
 		
 		current_df = pd.DataFrame(list(zip(all_val, train_labels)), 
 							   columns =[i, 'label'])
-		print('current unsorted dataframe for column ' + i + ' is:')
-		print(current_df)
+		#print('current unsorted dataframe for column ' + i + ' is:')
+		#print(current_df)
 		current_df.sort_values(by=[i], ascending = True , inplace = True)
 		current_df.reset_index(drop = True, inplace = True )
-		print('current sorted dataframe is:')
-		print(current_df)
-		print('Ready to enter find_current_best_split function...\n')		
+		#print('current sorted dataframe is:')
+		#print(current_df)
+		#print('Ready to enter find_current_best_split function...\n')		
 		current_best_split, curr_split_entropy = find_current_best_split(current_df)
-		if(current_best_split == len(train_df) - 1 ):
+	
+		if(current_best_split == -1):
 			continue
-		
 		if ((curr_split_entropy < least_curr_entropy) and (first == False)):
 			least_curr_entropy = curr_split_entropy
 			best_split['attr'] = i
@@ -109,26 +117,31 @@ def find_split(train_df):
 			best_split['value'] = current_df[i].iloc[current_best_split]
 			best_split['index'] = current_best_split
 			first = False
-		print('\n')
-	print( 'The least entropy found is ' + str(least_curr_entropy) + ' on attribute ' + str(best_split['attr']) + '\n' )
+		#print('\n')
+	#print( 'The least entropy found is ' + str(least_curr_entropy) + ' on attribute ' + str(best_split['attr']) + '\n' )
 	return best_split
 		
 def decision_tree_learning(train_df, depth):
 	global decided_splits_list
 	global label
-	print('The train dataframe is:')
-	print(train_df)
+	#print('The train dataframe is:')
+	#print(train_df)
 	print('\n')
-	if(train_df.loc[:,label].var()==0):
+	
+	if((train_df[label].nunique() == 1)):
 		temp_list = train_df[label]
-		print('temp_list:')
-		print(temp_list)
+		#print('temp_list:')
+		#print(temp_list)
 		return ({'attr' : label , 'value' : temp_list[0], 'left' : None, 'right' : None, 'leaf' : True }, depth)
 	else:
 
-		print('ready to enter find split function...\n')
+		#print('ready to enter find split function...\n')
 			
 		split = find_split (train_df)
+		if (split['attr'] == None):
+			value = train_df[label].value_counts().argmax()
+			return ({'attr' : label , 'value' : value, 'left' : None, 'right' : None, 'leaf' : True }, depth)
+
 		print('the following split is to take place:')
 		print(split)
 		decided_splits_list.append((str(split['attr']) + '<' + str(split['value'])))
@@ -136,9 +149,9 @@ def decision_tree_learning(train_df, depth):
 		print(decided_splits_list)
 		print('\n')
 		# print('split found')
-		virtual_node = {'attr' : split['attr'] , 'value' : split['value'], 'left' : 'TBD', 'right' : 'TBD', 'depth' : depth, 'leaf' : False }
+		#virtual_node = {'attr' : split['attr'] , 'value' : split['value'], 'left' : 'TBD', 'right' : 'TBD', 'depth' : depth, 'leaf' : False }
 		time_stamp = time.time() - start
-		print(virtual_node)
+		#print(virtual_node)
 		print('time elapsed:')
 		print(time_stamp)
 		print('\n')
@@ -146,26 +159,26 @@ def decision_tree_learning(train_df, depth):
 
 		temp_df = train_df.sort_values(by=[split['attr']], ascending = True )
 		temp_df.reset_index(drop = True, inplace = True )	
-		print('Sorted dataframe in respect to ' + str(split['attr'])+ ' :')
-		print(temp_df)
-		print('\n')
-		print('split at index:')
-		print(split['index'])
+		#print('Sorted dataframe in respect to ' + str(split['attr'])+ ' :')
+		#print(temp_df)
+		#print('\n')
+		#print('split at index:')
+		#print(split['index'])
 		l_dataset = temp_df.iloc[ : split['index'], :]
-		l_rows = len(l_dataset[split['attr']])
+		#l_rows = len(l_dataset[split['attr']])
 		# l_dataset = l_dataset.drop(split['attr'])
 		
 		r_dataset = temp_df.iloc[split['index']:, :]
 		r_dataset.reset_index(drop = True, inplace = True)
 		
-		r_rows = len(r_dataset[split['attr']])
+		#r_rows = len(r_dataset[split['attr']])
 		# r_dataset = r_dataset.drop(split['attr'])
-		print('rows in left dataset:')
-		print(l_rows)
-		print('rows in right dataset:')
-		print(r_rows)
+		#print('rows in left dataset:')
+		#print(l_rows)
+		#print('rows in right dataset:')
+		#print(r_rows)
 		
-		print('\n' * 15)
+		#print('\n' * 15)
 		l_branch, l_depth = decision_tree_learning(l_dataset, depth + 1)
 		r_branch, r_depth = decision_tree_learning(r_dataset, depth + 1)
 		
@@ -176,7 +189,7 @@ def decision_tree_learning(train_df, depth):
 #################################################################################  MAIN  ############################################################################
 start = time.time()
 print ("hello")
-clean_array = np.loadtxt('small_dataset.txt')
+clean_array = np.loadtxt('clean_dataset.txt')
 attr_list = ['wifi1', 'wifi2', 'wifi3','wifi4', 'wifi5', 'wifi6', 'wifi7', 'room']
 label = 'room'
 train_df = pd.DataFrame(clean_array, columns = attr_list)
