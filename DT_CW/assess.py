77bd13b523549c098068a7f905fd8248a91bbaf2
Alexandros Constantinou <52408185+ac56@users.noreply.github.com>
2019-07-23 18:30:22 +0300

    Create assess.py

---
+++ DT_CW/assess.py
@@ -0,0 +1,17 @@
+import setup
+
+def set_score ( prediction_list, answers ):
+    # print('\n' + 'entered set_score function')
+    # print('prediction list:')
+    # print(prediction_list)
+    # print('answers:')
+    # print(answers)
+    correct_predictions = 0
+    if ( len(prediction_list) != len(answers) ):
+        return "Cannot assess prediction list as it has a different length to the answers list"
+    else:
+        for i in range(len(prediction_list)):
+            if ( prediction_list[i] == answers[i]):
+                correct_predictions += 1
+    return (correct_predictions/len(prediction_list)) * 100
+
