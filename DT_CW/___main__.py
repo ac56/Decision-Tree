21a04f9c44a689f507423ac50dd1084b8252ad29
Alexandros Constantinou <52408185+ac56@users.noreply.github.com>
2019-07-24 18:30:57 +0300

    Update ___main__.py

--- DT_CW/___main__.py
+++ DT_CW/___main__.py
@@ -2,6 +2,7 @@ import time
 import setup
 import build_tree
 import evaluation
+import pruning
 
 start = time.time()
 print ("hello")
@@ -12,5 +13,10 @@ print(time_stamp)
 print('\n')
 #decided_splits_list = []
 #print(tree)
-
-evaluation.evaluate_tree( setup.train_df, 10)
+prune_list = []
+prune_list = pruning.find_potential_prunes( tree , prune_list)
+print(('\n' * 4) + 'prune_list:')
+for i in range(len(prune_list)):
+    print(prune_list[i])
+    print('\n')
+#evaluation.evaluate_unpruned( setup.train_df, 10)
