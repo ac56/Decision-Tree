39611b01f0282863ec03697af63f7527785f896f
Alexandros Constantinou <52408185+ac56@users.noreply.github.com>
2019-07-25 18:34:40 +0300

    Update evaluation.py

--- DT_CW/evaluation.py
+++ DT_CW/evaluation.py
@@ -4,6 +4,7 @@ import build_tree
 import predict
 import assess
 import pruning
+from termcolor import colored
 
 def evaluate_unpruned (train_df, n_partitions):
     print('\n')
@@ -28,6 +29,7 @@ def evaluate_unpruned (train_df, n_partitions):
         train_part.reset_index(drop = True, inplace = True)
         answers = test_part[setup.label]
         answers.reset_index(drop = True , inplace = True)
+        answers = answers.tolist()
         test_part = test_part.drop([setup.label], axis = 1)
         tree_info = tuple()
         tree_info = build_tree.decision_tree_learning(train_part, 0)
@@ -43,8 +45,8 @@ def evaluate_unpruned (train_df, n_partitions):
         score = assess.set_score (prediction_list, answers)
         scores_total += score
         #print ( 'score for test_data with index ' + str(test_begin) + ' to ' + str(test_end) + ' is ' + str (score) + '%')
-    print (('\n' * 5) +  'the average depth is ' + str(depths_total/ n_partitions))
-    print ('the average score for unpruned tree is ' + str(scores_total/n_partitions) + '%')
+    print (colored(('\n' * 5) +  'the average depth is ' + str(depths_total/ n_partitions), 'red'))
+    print (colored('the average score for unpruned tree is ' + str(scores_total/n_partitions) + '%', 'red'))
 
 def evaluate_pruned (train_df, n_partitions):
     print('\n')
@@ -60,6 +62,8 @@ def evaluate_pruned (train_df, n_partitions):
         # print('test_begin:\t' + str(test_begin))
         # print('test_end:\t' + str(test_end))
         test_part = train_df.iloc[test_begin:test_end, :]
+        # print('testing part of dataset:')
+        # print( test_part)
         train_part = train_df.drop(train_df.index[test_begin:test_end])
         # print('the test part is:')
         # print(test_part)
@@ -67,14 +71,17 @@ def evaluate_pruned (train_df, n_partitions):
         # print(train_part)
         # print('\n' * 5)
         if ((test_begin - part_len) < 0 ):
-            valid_part = train_df.iloc[-part_len]
+            valid_part = train_df.iloc[-part_len:, :]
         else:
             valid_part = train_df.iloc[test_begin-part_len:test_begin, :]
+        # print('validation part of dataset:')
+        # print( valid_part)
         train_part = train_part.drop(train_part.index[test_begin-part_len:test_begin])
         train_part.reset_index(drop = True, inplace = True)
         tree_info = tuple()
         tree_info = build_tree.decision_tree_learning(train_part, 0)
         depths_total += tree_info[1]
+        tree_info = list(tree_info)
         tree_info[0] = pruning.prune_tree (tree_info[0], valid_part)
         # print('current tree:')
         # print(tree_info[0])
@@ -93,6 +100,7 @@ def evaluate_pruned (train_df, n_partitions):
 ########################################################################################################################################### TESTING
         test_answers = test_part[setup.label]
         test_answers.reset_index(drop = True , inplace = True)
+        test_answers = test_answers.tolist()
         test_part = test_part.drop([setup.label], axis = 1)
         test_prediction_list = []
         for index, row in test_part.iterrows():
@@ -101,5 +109,5 @@ def evaluate_pruned (train_df, n_partitions):
         score = assess.set_score (test_prediction_list, test_answers)
         scores_total += score
         #print ( 'score for test_data with index ' + str(test_begin) + ' to ' + str(test_end) + ' is ' + str (score) + '%')
-    print (('\n' * 5) +  'the average depth is ' + str(depths_total/ n_partitions))
-    print ('the average score is ' + str(scores_total/n_partitions) + '%')
+    print (colored(('\n' * 5) +  'the average depth is ' + str(depths_total/ n_partitions),'red'))
+    print (colored('the average score for pruned tree is ' + str(scores_total/n_partitions) + '%', 'red'))
