ead3bf29c12344b200a36b7916237bd36e4f75ef
Alexandros Constantinou <52408185+ac56@users.noreply.github.com>
2019-07-25 18:34:32 +0300

    Update setup.py

--- DT_CW/setup.py
+++ DT_CW/setup.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-clean_array = np.loadtxt('smaller_dataset.txt')
+clean_array = np.loadtxt('noisy_dataset.txt')
 attr_list = ['wifi1', 'wifi2', 'wifi3','wifi4', 'wifi5', 'wifi6', 'wifi7', 'room']
 label = 'room'
 train_df = pd.DataFrame(clean_array, columns = attr_list)
