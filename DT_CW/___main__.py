4211b6787c0bfcb1cca01cb773c0b9143646f24d
Alexandros Constantinou <52408185+ac56@users.noreply.github.com>
2019-07-25 18:34:50 +0300

    Update ___main__.py

--- DT_CW/___main__.py
+++ DT_CW/___main__.py
@@ -4,19 +4,16 @@ import build_tree
 import evaluation
 import pruning
 
-start = time.time()
-print ("hello")
-tree, depth = build_tree.decision_tree_learning(setup.train_df, setup.depth)
-time_stamp = time.time() - start
-print('time elapsed to build tree:')
-print(time_stamp)
-print('\n')
-#decided_splits_list = []
-#print(tree)
-prune_list = []
-prune_list = pruning.find_potential_prunes( tree , prune_list)
-print(('\n' * 4) + 'prune_list:')
-for i in range(len(prune_list)):
-    print(prune_list[i])
-    print('\n')
-#evaluation.evaluate_unpruned( setup.train_df, 10)
+# start = time.time()
+# print ("hello")
+# tree, depth = build_tree.decision_tree_learning(setup.train_df, setup.depth)
+# time_stamp = time.time() - start
+# print('time elapsed to build tree:')
+# print(time_stamp)
+# print('\n')
+# decided_splits_list = []
+# print(tree)
+
+# evaluation.evaluate_unpruned( setup.train_df, 10)
+
+evaluation.evaluate_pruned( setup.train_df, 10)
